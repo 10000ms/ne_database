a53e5a3a2906e32ab78e4dbf66d4ed665d850f3d
1000ms <victor.lai@foxmail.com>
2023-05-18 23:58:30 +0800

    update: 添加单元测试

--- core/table_schema/table_test.go
+++ core/table_schema/table_test.go
@@ -1,6 +1,7 @@
 package tableSchema
 
 import (
+	"ne_database/core/base"
 	"testing"
 )
 
@@ -27,5 +28,32 @@ func TestFieldInfoVerification(t *testing.T) {
 		t.Error("TestFieldInfo_Verification() 测试用例2未通过，期望结果是类型长度错误，但得到了没有错误")
 	} else if err.Error() != "int64类型长度错误: 4" {
 		t.Errorf("TestFieldInfo_Verification() 测试用例2未通过，期望的错误信息是\"int64类型长度错误: 4\"，但得到了%s", err.Error())
+	}
+}
+
+func TestRawToFieldType(t *testing.T) {
+	typeTest, err := RawToFieldType("int64")
+	if err != nil {
+		t.Errorf("[RawToFieldType] The test case did not pass, expecting no errors but received an error: %s", err.Error())
+	}
+	if typeTest != Int64Type {
+		t.Errorf("[RawToFieldType] test fail, expect: %v, got: %v", Int64Type, typeTest)
+	}
+
+	typeTest, err = RawToFieldType("string")
+	if err != nil {
+		t.Errorf("[RawToFieldType] The test case did not pass, expecting no errors but received an error: %s", err.Error())
+	}
+	if typeTest != StringType {
+		t.Errorf("[RawToFieldType] test fail, expect: %v, got: %v", StringType, typeTest)
+	}
+
+	typeTest, err = RawToFieldType("errorType")
+	expErr := base.NewDBError(base.FunctionModelCoreDTableSchema, base.ErrorTypeInput, base.ErrorBaseCodeInnerParameterError, nil)
+	if err == nil {
+		t.Error("[RawToFieldType] test fail, expected result was an error, but there was no error received")
+	}
+	if err.GetErrorCode() != expErr.GetErrorCode() {
+		t.Errorf("[RawToFieldType] expect error: %s, got err: %s", expErr, err)
 	}
 }
